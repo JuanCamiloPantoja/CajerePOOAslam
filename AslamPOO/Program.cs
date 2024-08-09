194f1551ed400c8266812505ce9aa575dd64ab9c
Ash20 <Ash20@CAMILO>
2024-08-09 16:50:59 -0500

    Se realiza commit dev

--- AslamPOO/Program.cs
+++ AslamPOO/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace CajeroAutomatico
+{
+    public class Cajero
+    {
+        static void Main(string[] args)
+        {
+            Cajero cajero = new Cajero("123456789", "clave123", 1000);
+
+            Console.WriteLine("Bienvenido al cajero automático");
+            Console.Write("Ingrese su número de cuenta: ");
+            string numeroCuentaIngresada = Console.ReadLine();
+            Console.Write("Ingrese su clave: ");
+            string claveIngresada = Console.ReadLine();
+
+            cajero.IniciarSesion(numeroCuentaIngresada, claveIngresada);
+        }
+
+        private string numeroCuenta;
+        private string clave;
+        private decimal saldo;
+    }
+}
